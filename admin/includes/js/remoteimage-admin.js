001a26bf02b35477b3abd12f47418f2e60c536bb
Simon Asika <asika32764@gmail.com>
2014-06-29 16:27:55 +0800

    Reformat code

--- admin/includes/js/remoteimage-admin.js
+++ admin/includes/js/remoteimage-admin.js
@@ -1,4 +1,4 @@
-/*!
+/**
  * com_remoteimage
  *
  * Copyright 2012 Asikart.com
@@ -9,97 +9,118 @@
  */
 
 var Remoteimage = {
-    insertImages : function(id, imgs, urls, elFinder, option){
-        var tags = '';
-        
-        // Handle From Urls
-        urls = urls.toString().trim();
 
-        if( urls ) {
-            urls = urls.split("\n");
-            
-            urls.each( function(e, i){
-                var path = e.split('/');
-                var ext = path.getLast().split('.').getLast();
-                var img_ext = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg'];
-                
-                if(!e.trim()) {
-                    return;
-                }
-                
-                if( img_ext.contains(ext.toLowerCase()) ) {
-                    // Create img element
-                    var img = new Element('img', {
-                        alt : path.getLast() ,
-                        src : e
-                    }) ;
-                    
-                    // Fix Width
-                    if( option.fixAll ) {
-                        img.set('width', option.dW) ;
-                    }
-                    
-                    tags += option.insert_template_image.replace( '{%CONTENT%}' ,img.outerHTML);
-                }else{
-                    var a = new Element('a', {
-                        href : e,
-                        target : '_blank',
-                        text : path.getLast()
-                    });
-                    
-                    tags += '&nbsp; ' + option.insert_template_link.replace( '{%CONTENT%}' ,a.outerHTML) + '&nbsp; ' ;
-                }
-                
-            });
-        }else{
-            // Insert From Selected
-            if( imgs.length < 1 ) {
-                return ;
-            }
-            
-            imgs.each( function(e, i){
+	/**
+	 * Insert images.
+	 *
+	 * @param id
+	 * @param imgs
+	 * @param urls
+	 * @param elFinder
+	 * @param option
+	 */
+	insertImages: function(id, imgs, urls, elFinder, option)
+	{
+		var tags = '';
 
-	            var url = elFinder.url(e.hash);
+		// Handle From Urls
+		urls = urls.toString().trim();
 
-	            if (url.indexOf(option.root_uri) == 0)
-	            {
-		            url = url.substr(option.root_uri.length);
-	            }
+		if (urls)
+		{
+			urls = urls.split("\n");
 
-	            console.log(url, option);
+			urls.each(function(e, i)
+			{
+				var path = e.split('/');
+				var ext = path.getLast().split('.').getLast();
+				var img_ext = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg'];
 
-                if( e.mime.split('/')[0] == 'image' ) {
-                    // Create img element
-                    var img = new Element('img', {
-                        alt : e.name ,
-                        src : url
-                    }) ;
-                    
-                    // Fix Width
-                    if( option.fixAll ) {
-                        img.set('width', option.dW) ;
-                    }
-                    
-                    tags += option.insert_template_image.replace( '{%CONTENT%}' ,img.outerHTML);
-                }else{
+				if (!e.trim())
+				{
+					return;
+				}
 
-                    var a = new Element('a', {
-                        href : url,
-                        target : '_blank',
-                        text : e.name
-                    });
-                    
-                    tags += '&nbsp; ' + option.insert_template_link.replace( '{%CONTENT%}' ,a.outerHTML) + '&nbsp; ' ;
-                }
-                
-            } );
-        }
-        
-        
-        jInsertEditorText(tags, id);
-        SqueezeBox.close();
+				if (img_ext.contains(ext.toLowerCase()))
+				{
+					// Create img element
+					var img = new Element('img', {
+						alt: path.getLast(),
+						src: e
+					});
 
-    }
-}
+					// Fix Width
+					if (option.fixAll)
+					{
+						img.set('width', option.dW);
+					}
 
+					tags += option.insert_template_image.replace('{%CONTENT%}', img.outerHTML);
+				}
+				else
+				{
+					var a = new Element('a', {
+						href: e,
+						target: '_blank',
+						text: path.getLast()
+					});
 
+					tags += '&nbsp; ' + option.insert_template_link.replace('{%CONTENT%}', a.outerHTML) + '&nbsp; ';
+				}
+
+			});
+		}
+		else
+		{
+			// Insert From Selected
+			if (imgs.length < 1)
+			{
+				return;
+			}
+
+			imgs.each(function(e, i)
+			{
+				var url = elFinder.url(e.hash);
+
+				if (url.indexOf(option.root_uri) == 0)
+				{
+					url = url.substr(option.root_uri.length);
+				}
+
+				console.log(url, option);
+
+				if (e.mime.split('/')[0] == 'image')
+				{
+					// Create img element
+					var img = new Element('img', {
+						alt: e.name,
+						src: url
+					});
+
+					// Fix Width
+					if (option.fixAll)
+					{
+						img.set('width', option.dW);
+					}
+
+					tags += option.insert_template_image.replace('{%CONTENT%}', img.outerHTML);
+				}
+				else
+				{
+
+					var a = new Element('a', {
+						href: url,
+						target: '_blank',
+						text: e.name
+					});
+
+					tags += '&nbsp; ' + option.insert_template_link.replace('{%CONTENT%}', a.outerHTML) + '&nbsp; ';
+				}
+
+			});
+		}
+
+		jInsertEditorText(tags, id);
+		SqueezeBox.close();
+	}
+};
