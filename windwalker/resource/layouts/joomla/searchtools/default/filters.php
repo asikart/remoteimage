5645dacd6ca269497b4d7b99b89a807b15881dea
Simon Asika <asika32764@gmail.com>
2016-03-27 03:06:00 +0800

    resource prepare

---
+++ windwalker/resource/layouts/joomla/searchtools/default/filters.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+$data = $displayData;
+
+// Load the form filters
+$filters = $data['view']->filterForm->getGroup('filter');
+$searches = $data['view']->filterForm->getFieldset('multisearch');
+?>
+
+<?php if ($searches) : ?>
+	<?php foreach ($searches as $fieldName => $field) : ?>
+		<div class="js-stools-field-filter">
+			<?php echo isset($displayData['option']['no_label']) ? $field->label : ''; ?>
+			<?php echo $field->input; ?>
+		</div>
+	<?php endforeach; ?>
+<?php endif; ?>
+
+<?php if ($filters) : ?>
+	<?php foreach ($filters as $fieldName => $field) : ?>
+		<div class="js-stools-field-filter">
+			<?php echo isset($displayData['option']['no_label']) ? $field->label : ''; ?>
+			<?php echo $field->input; ?>
+		</div>
+	<?php endforeach; ?>
+<?php endif; ?>
