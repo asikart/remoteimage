42510b13bfc187a8edb6be08bbf52029320a7915
Simon Asika <asika32764@gmail.com>
2016-07-21 07:51:48 +0800

    Fix routing issues

--- site/router.php
+++ site/router.php
@@ -20,51 +20,95 @@ $router = CmsRouter::getInstance('com_remoteimage');
 // Register routing config and inject Router object into it.
 $router = RoutingHelper::registerRouting($router, 'com_remoteimage');
 
-/**
- * RemoteimageBuildRoute
- *
- * @param array &$query
- *
- * @return  array
- */
-function RemoteimageBuildRoute(&$query)
+if (!function_exists('RemoteimageBuildRoute'))
 {
-	$segments = array();
-
-	$router = CmsRouter::getInstance('com_remoteimage');
-
-	$query = \Windwalker\Router\Route::build($query);
-
-	if (!empty($query['view']))
+	/**
+	 * RemoteimageBuildRoute
+	 *
+	 * @param array &$query
+	 *
+	 * @return  array
+	 */
+	function RemoteimageBuildRoute(&$query)
 	{
-		$segments = $router->build($query['view'], $query);
+		$segments = array();
 
-		unset($query['view']);
+		$router = CmsRouter::getInstance('com_remoteimage');
+
+		$query = \Windwalker\Router\Route::build($query);
+
+		if (!empty($query['view']))
+		{
+			$segments = $router->build($query['view'], $query);
+
+			unset($query['view']);
+		}
+
+		return $segments;
 	}
-
-	return $segments;
 }
 
-/**
- * RemoteimageParseRoute
- *
- * @param array $segments
- *
- * @return  array
- */
-function RemoteimageParseRoute($segments)
+if (!function_exists('RemoteimageParseRoute'))
 {
-	$router = CmsRouter::getInstance('com_remoteimage');
-
-	$segments = implode('/', $segments);
-
-	// OK, let's fetch view name.
-	$view = $router->getView(str_replace(':', '-', $segments));
-
-	if ($view)
+	/**
+	 * RemoteimageParseRoute
+	 *
+	 * @param array $segments
+	 *
+	 * @return  array
+	 */
+	function RemoteimageParseRoute($segments)
 	{
-		return array('view' => $view);
-	}
+		$router = CmsRouter::getInstance('com_remoteimage');
 
-	return array();
+		$segments = implode('/', $segments);
+
+		// OK, let's fetch view name.
+		$view = $router->getView(str_replace(':', '-', $segments));
+
+		if ($view)
+		{
+			return array('view' => $view);
+		}
+
+		return array();
+	}
+}
+
+if (class_exists('JComponentRouterBase'))
+{
+	class RemoteimageRouter extends JComponentRouterBase
+	{
+		/**
+		 * Build method for URLs
+		 * This method is meant to transform the query parameters into a more human
+		 * readable form. It is only executed when SEF mode is switched on.
+		 *
+		 * @param   array &$query An array of URL arguments
+		 *
+		 * @return  array  The URL arguments to use to assemble the subsequent URL.
+		 *
+		 * @since   2.0.8
+		 */
+		public function build(&$query)
+		{
+			return RemoteimageBuildRoute($query);
+		}
+
+		/**
+		 * Parse method for URLs
+		 * This method is meant to transform the human readable URL back into
+		 * query parameters. It is only executed when SEF mode is switched on.
+		 *
+		 * @param   array &$segments The segments of the URL to parse.
+		 *
+		 * @return  array  The URL attributes to be used by the application.
+		 *
+		 * @since   2.0.8
+		 */
+		public function parse(&$segments)
+		{
+			return RemoteimageParseRoute($segments);
+		}
+	}
 }
