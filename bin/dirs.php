29131b15ae65a96ab1e319a4a95e45fe5a19b8e9
Simon Asika <asika32764@gmail.com>
2018-11-19 21:54:54 +0800

    Use build script

---
+++ bin/dirs.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Part of Component Ezset files.
+ *
+ * @copyright   Copyright (C) 2017 Asikart.co. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+const _JEXEC = 1;
+defined('_JEXEC') or die;
+
+return array(
+	'components/com_remoteimage' => 'site',
+    'administrator/components/com_remoteimage' => 'admin',
+    'plugins/editors-xtd/remoteimage' => 'plugins/plg_editors-xtd_remoteimage',
+    'plugins/system/remoteimage' => 'plugins/plg_system_remoteimage'
+);
